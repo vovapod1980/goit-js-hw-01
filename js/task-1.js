a3405d45e59cd7a956268e9b7a66c5b67fb37a20
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-01-06 22:00:51 +0200

    hw js

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
-    // ....
+  return `You ordered ${quantity} droids worth ${
+    quantity * pricePerDroid
+  } credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
